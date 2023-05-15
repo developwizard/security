0103c99ad326db1498b7d80de4792d47c9f4b46a
dewiz <churkin.it@gmail.com>
2023-05-15 20:40:31 +0300

    Create UserDetails

---
+++ src/main/java/com/dewiz/security/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.dewiz.security.user;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    Optional<User> findByEmail(String email);
+}
